2a61a8a72df44bca06d0d5b6d25bed533de72cfa
JuntaoX <18jx10@queensu.ca>
2022-03-02 11:42:23 -0500

    daily Q258 create submit solution_juntao

--- get.py
+++ get.py
@@ -119,7 +119,7 @@ def gen_markdown(path, content, title, url):
     file.close()
 
 
-url = "https://leetcode-cn.com/problems/find-the-closest-palindrome/"
+url = "https://leetcode-cn.com/problems/add-digits/"
 
 slug = url.replace("https://leetcode-cn.com/problems/", "", 1).strip('/')
 question = get_all(slug=slug)
